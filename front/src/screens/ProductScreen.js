4f3717de663762275a6064619412744d14c7aa90
Berlin Johnson <berlinjohnson@Berlins-MacBook-Pro-2.local>
2020-12-31 00:19:28 +0900

    Product Screen

--- front/src/screens/ProductScreen.js
+++ front/src/screens/ProductScreen.js
@@ -1,7 +1,14 @@
 /*
-
+  1. Import {Link} from 'react-router-dom'
+  2. Import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap
+  3. Import Rating 
+  4. Import products from '../products'
 */
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import Rating from './components/Rating'
+import products from '../products'
 
 const ProductScreen = () => {
   return (
