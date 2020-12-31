2b308f1ba6043d050888432e5cabf26588798a41
Berlin Johnson <berlinjohnson@Berlins-MacBook-Pro-2.local>
2020-12-31 21:06:55 +0900

    Finished fetching server with express

--- backend/data/products.js
+++ backend/data/products.js
@@ -86,4 +86,4 @@ const products = [
 
 ];
 
-module.exports =  products
+export default products
--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,22 @@
   6. Inside /data/products.js Change 'export default' to 'module.exports = '
   7. Set products = require('./data/products')
   8. Set app.get('/api/products/:id', (req, res) => { const product = products.find(p => p._id === req.params.id)}) res.json(products)-> Restart server
+  9. Bring in Environment Variables-> set dotenv = require('dotenv')
+  10. Declare dotenv.config()-> create .env file->restart server
+  11. Declare const PORT = process.env.PORT || 5000
+  12. Change(PORT, `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  13. After 'type': 'module' in package.json-> Change all require to import express from 'express'
+  14. Go to products.js and change to export default products
+  15. Add .js to all imported files
 */
 
-const express = require('express')
-
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 const app = express()
+
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Api running......')
@@ -28,4 +37,7 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
-app.listen(5000, console.log('Server is running on port 5000'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
