96f35b3f54228c5f1ea5b160ac38f70a5e87a457
Berlin Johnson <berlinjohnson@Berlins-MacBook-Pro-2.local>
2020-12-31 15:26:08 +0900

    Backend setup

---
+++ backend/data/products.js
@@ -0,0 +1,89 @@
+/*
+  1. Create a products array with product info
+  2. export default products bottom of page
+*/
+
+const products = [
+
+  {
+    _id: '1',
+    name: 'Airpods Wireless Bluetooth Headphones', 
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 139.99,
+      countInStock: 3,
+      rating: 4.5,
+      numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'Amazon Echo Dot 3rd Generation', 
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      brand: 'Amazon',
+      category: 'Electronics',
+      price: 29.99,
+      countInStock: 0,
+      rating: 4,
+      numReviews: 4,
+  },
+  {
+    _id: '3',
+    name: 'Cannon EOS 80D DSLR Camera', 
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      brand: 'Cannon',
+      category: 'Electronics',
+      price: 989.99,
+      countInStock: 13,
+      rating: 4.7,
+      numReviews: 6,
+  },
+  {
+    _id: '4',
+    name: 'Logitect G-Series Gaming Mouse', 
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      brand: 'Logitech',
+      category: 'Electronics',
+      price: 49.99,
+      countInStock: 7,
+      rating: 3.8,
+      numReviews: 2,
+  },
+  {
+    _id: '5',
+    name: 'iPhone 11 Pro 256GB Memory', 
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecendented leap in battery life',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 599.99,
+      countInStock: 20,
+      rating: 4.3,
+      numReviews: 9,
+  },
+  {
+    _id: '6',
+    name: 'Sony Playstation 4 Pro White Version', 
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with Playstation. Whether you are into gaming, HD movies, television or music',
+      brand: 'Sony',
+      category: 'Electronics',
+      price: 399.99,
+      countInStock: 0,
+      rating: 4.9,
+      numReviews: 9,
+  },
+
+];
+
+module.exports =  products
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+/*
+  1. Set const express = require('express')
+  2. Set app = express();
+  3. Set app.listen(5000, console.log('Server running...))
+  4. In package.json, inside scripts change test to start and value to 'node backend/server to run server with 'npm start'
+  5. Set app.get('/', (req, res) => { res.send('API is running...)})
+  5. Set app.get('/api/products', (req, res) => { res.json(products)})
+  6. Inside /data/products.js Change 'export default' to 'module.exports = '
+  7. Set products = require('./data/products')
+  8. Set app.get('/api/products/:id', (req, res) => { const product = products.find(p => p._id === req.params.id)}) res.json(products)-> Restart server
+*/
+
+const express = require('express')
+
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Api running....')
+})
+// create json api from products.js and get on site
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+// create route to get single product by id
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+app.listen(5000, console.log('Server is running on port 5000'))
